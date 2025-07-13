0dd1ac24f34c6dd79761fd666ef0d7656ad9ca0a
mj-akhlaghi77 <mj.akhlaghi7@gmail.com>
2025-07-13 17:17:29 +0330

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ async def crypto_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text("⚠️ خطا در دریافت اطلاعات ارز. لطفاً دوباره تلاش کن.")
 
 # Run bot
-if name == "__main__":
+if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
